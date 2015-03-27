3f8abcebefa9cfc9357fe6eb28a648f0c9ff67b1
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2015-03-27 15:42:18 -0700

    doubleclick is serving over https

--- default_filters.js
+++ default_filters.js
@@ -1,7 +1,7 @@
 defaultFilters = [
 	// personally, I can't stand the like box
 	//"http://www.facebook.com/plugins/likebox.php?*",
-	"http://*.doubleclick.net/*",
+	"*://*.doubleclick.net/*",
 	"*://*.googleadservices.com/*",
 	"*://*.googlesyndication.com/*",
 	"*://*.google-analytics.com/*",
