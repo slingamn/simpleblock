ef916ec304dead04f2dd674c895123adf9eddf67
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2015-09-10 01:53:44 -0700

    unblock full-page googleadservices (sponsored results in google search)

--- default_filters.js
+++ default_filters.js
@@ -2,7 +2,7 @@ defaultFilters = [
 	// personally, I can't stand the like box
 	//"http://www.facebook.com/plugins/likebox.php?*",
 	"*://*.doubleclick.net/*",
-	"*://*.googleadservices.com/*",
+	"*://partner.googleadservices.com/*",
 	"*://*.googlesyndication.com/*",
 	"*://*.google-analytics.com/*",
 	"*://creative.ak.fbcdn.net/*",
